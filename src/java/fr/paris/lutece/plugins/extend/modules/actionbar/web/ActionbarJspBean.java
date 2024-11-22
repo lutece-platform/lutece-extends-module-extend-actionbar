b0238017c54a6e6179cd1e4e961eeaa188b8a130
sebastien leridon <sebleridon@laposte.net>
2024-11-22 10:49:17 +0100

    Update global pom and commons.lang3 package

--- src/java/fr/paris/lutece/plugins/extend/modules/actionbar/web/ActionbarJspBean.java
+++ src/java/fr/paris/lutece/plugins/extend/modules/actionbar/web/ActionbarJspBean.java
@@ -64,7 +64,7 @@ import java.util.Map;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 
 /**
